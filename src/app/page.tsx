3d761a6410619187748d4144b60b14c577f04502
akhil7168/MinorProject <akhilpunyala@gmail.com>
2026-03-20 22:38:12 +0530

    fix: add use client directive to pages and fix cross-origin config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 
 const DashboardContent = dynamic(() => import('@/components/DashboardContent'), {
--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 
 const WorkoutsContent = dynamic(() => import('@/components/WorkoutsContent'), {
